5bc9ae47fc5028398fc45c704b79e8bc76d3e194
W <1259899074@qq.com>
2026-01-16 14:56:56 +0800

    封装Redis工具类

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,10 +1,20 @@
 package com.hmdp;
 
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.Shop;
+import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.hmdp.service.impl.ShopServiceImpl;
+import com.hmdp.utils.CacheClient;
+import com.hmdp.utils.RedisData;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
@@ -12,9 +22,22 @@ class HmDianPingApplicationTests {
     @Autowired
     ShopServiceImpl shopService;
 
+    @Autowired
+    CacheClient cacheClient;
+
+    @Autowired
+    ShopMapper shopMapper;
+
     @Test
     void saveShop(){
         shopService.saveShopDataToRedis(1L,10L);
+    }
+
+    @Test
+    void saveData(){
+        Shop shop = shopMapper.queryShopById(1L);
+
+        cacheClient.setWithLogicalExpire(CACHE_SHOP_KEY+1L, shop,10L, TimeUnit.SECONDS);
     }
 
 }
